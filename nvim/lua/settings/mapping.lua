dc6d985268d14589e2c9d6c0ae4f8c65c167dd5c
sqrtthree <imsqrtthree@gmail.com>
2022-09-20 15:50:15 +0800

    feat(nvim): add keymap to select all text in current buffer

--- nvim/lua/settings/mapping.lua
+++ nvim/lua/settings/mapping.lua
@@ -1,2 +1,5 @@
 -- Map Leader + s to save the file.
 vim.keymap.set('n', '<leader>s', '<cmd>write<cr>')
+
+-- Select all text in current buffer
+vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
