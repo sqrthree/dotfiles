cf749c507c27e50411a5c6a3b3d0c0d695fb1864
sqrtthree <imsqrtthree@gmail.com>
2022-09-21 21:43:07 +0800

    feat(nvim): show relative path of filename in Statusline

--- nvim/lua/plugins/ui/config.lua
+++ nvim/lua/plugins/ui/config.lua
@@ -1,8 +1,29 @@
 local config = {}
 
 function config.lualine()
+  local initial = function(str)
+    return str:sub(1,1)
+  end
+
   require('lualine').setup {
-    options = { theme = 'solarized_dark' }
+    options = {
+      theme = 'solarized_dark',
+      fmt = string.lower,
+    },
+    sections = {
+      lualine_a = {
+        {
+          'mode',
+          fmt = initial,
+        }
+      },
+      lualine_c = {
+        {
+          'filename',
+          path = 1,
+        }
+      },
+    }
   }
 end
 
