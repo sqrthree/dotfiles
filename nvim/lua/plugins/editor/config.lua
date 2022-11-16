da872389262533ab4b6302b17d6aefa412693e4b
sqrtthree <imsqrtthree@gmail.com>
2022-11-16 23:42:32 +0800

    feat: disable slow treesitter highlight for large files

--- nvim/lua/plugins/editor/config.lua
+++ nvim/lua/plugins/editor/config.lua
@@ -44,6 +44,15 @@ function config.nvim_treesitter()
     highlight = {
       enable = true,
       use_languagetree = true,
+      additional_vim_regex_highlighting = false,
+      -- To disable slow treesitter highlight for large files.
+      disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
     },
     indent = {
       enable = true,
