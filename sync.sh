3368e3a50294653085a067b88c8c0364116abbe3
Lin He <linhe0x0@gmail.com>
2023-02-27 23:00:00 +0800

    chore: update sync script to add support for copying specified target

--- sync.sh
+++ sync.sh
@@ -7,19 +7,93 @@
 
 set -e
 
-log () {
-  echo "=> $1"
+#
+# Vars
+#
+
+TARGET=$1
+
+if [ -z "$TARGET" ]; then
+  TARGET="all"
+fi
+
+#
+# Functions
+#
+
+# log a message with info level.
+info() {
+  echo "\033[34m[*] $1\033[0m"
 }
 
-log "[vim] sync changes..."
-cp ~/.vimrc ./vim/vimrc
-log "[vim] ok"
+# log a message with warning level.
+warn() {
+  echo "\033[33m[!] $1\033[0m"
+}
 
-log "[nvim] sync changes..."
-rm -rf ./nvim
-cp -r ~/.config/nvim ./nvim
-log "[nvim] ok"
+# log a message with success level.
+success() {
+  echo "\033[32m[✔] $1\033[0m"
+}
 
-log "[alacritty] sync changes..."
-cp  ~/.config/alacritty/alacritty.yml ./alacritty/alacritty.yml
-log "[alacritty] ok"
+# log a message with error level.
+error() {
+  echo "\033[31m[x] $1\033[0m"
+}
+
+# log sync message.
+sync() {
+  info "[$1] sync changes..."
+}
+
+# log ok message.
+ok() {
+  success "[$1] ok"
+}
+
+# sync vim configurations.
+sync_vim() {
+  sync "vim"
+  cp ~/.vimrc ./vim/vimrc
+  ok "vim"
+}
+
+# sync neovim configurations.
+sync_nvim() {
+  sync "neovim"
+  rm -rf ./nvim
+  cp -r ~/.config/nvim ./nvim
+  ok "neovim"
+}
+
+# sync alacritty configurations.
+sync_alacritty() {
+  sync "alacritty"
+  cp  ~/.config/alacritty/alacritty.yml ./alacritty/alacritty.yml
+  ok "alacritty"
+}
+
+#
+# Main
+#
+
+case "$TARGET" in
+  "vim")
+    sync_vim
+    ;;
+  "nvim")
+    sync_nvim
+    ;;
+  "alacritty")
+    sync_alacritty
+    ;;
+  "all")
+    sync_vim
+    sync_nvim
+    sync_alacritty
+    ;;
+  *)
+    error "Invalid target"
+    exit 1
+    ;;
+esac
