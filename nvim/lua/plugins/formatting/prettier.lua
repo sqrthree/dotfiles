b0cd05366debe7be22d4b46fd70bb38217cfd647
Lin He <linhe0x0@gmail.com>
2023-04-26 00:22:47 +0800

    feat(nvim): disable auto focus on the quickfix when there are errors

--- nvim/lua/plugins/formatting/prettier.lua
+++ nvim/lua/plugins/formatting/prettier.lua
@@ -5,12 +5,17 @@ return {
     { "<leader>p", "<cmd>PrettierAsync<cr>", mode = "n", silent = true, noremap = true },
   },
   config = function()
+    -- Disable auto formatting for files without "@format" or "@prettier" tag by 
+    -- default. The formatting still can be triggered by commands.
+    vim.api.nvim_set_var("prettier#autoformat_require_pragma", 1)
     -- Setup autoformatting files based on whether a config file can be found in
     -- the current directory or any parent directory.
-    vim.api.nvim_set_var("prettier#autoformat_require_pragma", 0)
     vim.api.nvim_set_var("prettier#autoformat_config_present", 1)
 
     -- Force the command `:Prettier` to async.
     vim.api.nvim_set_var("prettier#exec_cmd_async", 1)
+
+    -- Disable auto focus on the quickfix when there are errors.
+    vim.api.nvim_set_var("prettier#quickfix_auto_focus", 0)
   end,
 }
