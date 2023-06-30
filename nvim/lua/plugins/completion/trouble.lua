b8486e0df48a4b9525294ac6cd3621071d4e60d2
Lin He <linhe0x0@gmail.com>
2023-06-30 11:18:05 +0800

    feat(nvim): integrate trouble.nvim go get a pretty list for showing diagnostics, references, telescope results, quickfix and location lists

---
+++ nvim/lua/plugins/completion/trouble.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    { "<leader>xx", "<cmd>TroubleToggle<cr>", mode = "n", silent = true, noremap = true },
+    { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", mode = "n", silent = true, noremap = true },
+    { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", mode = "n", silent = true, noremap = true },
+    { "<leader>xl", "<cmd>TroubleToggle loclist<cr>", mode = "n", silent = true, noremap = true },
+    { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", mode = "n", silent = true, noremap = true },
+    { "<leader>gR", "<cmd>TroubleToggle lsp_references<cr>", mode = "n", silent = true, noremap = true },
+  },
+}
