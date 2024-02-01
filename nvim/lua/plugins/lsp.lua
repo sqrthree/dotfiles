f3cce72e16987ee1c39ea64a5871e13e4a4ea1b9
Lin He <linhe0x0@gmail.com>
2024-02-01 17:24:16 +0800

    feat(nvim): add support for lsp inlay hints

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -49,13 +49,19 @@ return {
       -- after the language server attaches to the current buffer
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-        callback = function(ev)
+        callback = function(args)
+          -- Enable the inlay hints by default.
+          -- local client = vim.lsp.get_client_by_id(args.data.client_id)
+          -- if client.server_capabilities.inlayHintProvider then
+          --   vim.lsp.inlay_hint.enable(args.buf, true)
+          -- end
+
           -- Enable completion triggered by <c-x><c-o>
-          vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+          vim.bo[args.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
           -- Buffer local mappings.
           -- See `:help vim.lsp.*` for documentation on any of the below functions
-          local opts = { buffer = ev.buf }
+          local opts = { buffer = args.buf }
           vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
           vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
           vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
