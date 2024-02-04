16444dc0662e3cf0f28967f85373fcbd2e77c462
Lin He <linhe0x0@gmail.com>
2024-02-05 00:11:43 +0800

    chore: link files by default

--- install.sh
+++ install.sh
@@ -16,10 +16,9 @@ source "${WORKING_DIR}"/utils.sh
 #
 
 TARGET=$1
-NOW=$(date +%Y%m%d%H%M%S)
 
 if [ -z "$TARGET" ]; then
-	TARGET="all"
+  TARGET="all"
 fi
 
 #
@@ -28,97 +27,20 @@ fi
 
 # log install message.
 install() {
-	info "[$1] Installing..."
+  info "[$1] Installing..."
 }
 
 # log ok message.
 ok() {
-	success "[$1] OK"
+  success "[$1] OK"
 }
 
-# backup files
-backup() {
-	if [ -e $2 ]; then
-		BACKUP_FILE_NAME="$2.$NOW.backup"
+# Prepare
+CONFIG_DIR=$HOME/.config
 
-		info "[$1] Detected that the configuration file already exists:"
-		info "[$1]   file: $2"
-		info "[$1] Trying to backup files..."
-		info "[$1]   $2 => $BACKUP_FILE_NAME"
-		mv $2 $BACKUP_FILE_NAME
-		info "[$1] The file has been backed up."
-	fi
-}
-
-# install vim configurations.
-install_vim() {
-	backup "vim" "$HOME/.vimrc"
-
-	install "vim"
-	cp ./vim/vimrc $HOME/.vimrc
-
-	ok "vim"
-}
-
-# install vim basic configurations.
-install_vim_basic() {
-	backup "vim-basic" "$HOME/.vimrc"
-
-	install "vim_basic"
-	cp ./vim/vimrc-basic $HOME/.vimrc
-
-	ok "vim-basic"
-}
-
-# install neovim configurations.
-install_nvim() {
-	backup "neovim" "$HOME/.config/nvim"
-
-	install "neovim"
-	if [ -d $HOME/.config/nvim ]; then
-		rm -r $HOME/.config/nvim
-	fi
-	cp -r ./nvim $HOME/.config/nvim
-
-	ok "neovim"
-}
-
-# install alacritty configurations.
-install_alacritty() {
-	backup "alacritty" "$HOME/.config/alacritty"
-
-	install "alacritty"
-	mkdir -p $HOME/.config/alacritty
-	cp ./alacritty/alacritty.toml $HOME/.config/alacritty/alacritty.toml
-
-	info "[alacritty] Installing alacritty theme..."
-	info "[alacritty]   for more information on available themes, please see https://github.com/alacritty/alacritty-theme#color-schemes"
-	git clone https://github.com/alacritty/alacritty-theme $HOME/.config/alacritty/alacritty-theme
-
-	ok "alacritty"
-}
-
-# install tmux configurations.
-install_tmux() {
-	backup "tmux" "$HOME/.tmux.conf"
-
-	install "tmux"
-	cp ./tmux/tmux.conf $HOME/.tmux.conf
-
-	ok "tmux"
-}
-
-# install starship configurations.
-install_starship() {
-	backup "starship" "$HOME/.config/starship.toml"
-
-	install "starship"
-
-	mkdir -p $HOME/.config
-	cp ./starship/starship.toml $HOME/.config/starship.toml
-
-	ok "starship"
-}
+if [[ ! -d "$CONFIG_DIR" ]]; then
+  mkdir -p $CONFIG_DIR
+fi
 
 # Check what shell is being used
 SH="${HOME}/.bashrc"
@@ -126,37 +48,96 @@ ZSHRC="${HOME}/.zshrc"
 SHELL_DIR="$HOME/.shell"
 
 if [ -f "$ZSHRC" ]; then
-	SH="$ZSHRC"
+  SH="$ZSHRC"
 fi
+
+# install vim configurations.
+install_vim() {
+  install "vim"
+
+  link_file $PWD/vim/vimrc $HOME/.vimrc
+
+  ok "vim"
+}
+
+# install vim basic configurations.
+install_vim_basic() {
+  install "vim_basic"
+
+  link_file $PWD/vim/vimrc-basic $HOME/.vimrc
+
+  ok "vim-basic"
+}
+
+# install neovim configurations.
+install_nvim() {
+  install "neovim"
+
+  link_file $PWD/nvim $HOME/.config/nvim
+
+  ok "neovim"
+}
+
+# install alacritty configurations.
+install_alacritty() {
+  install "alacritty"
+
+  mkdir -p $HOME/.config/alacritty
+  link_file $PWD/alacritty/alacritty.toml $HOME/.config/alacritty/alacritty.toml
+
+  info "[alacritty] Installing alacritty theme..."
+  info "[alacritty]   for more information on available themes, please see https://github.com/alacritty/alacritty-theme#color-schemes"
+  git clone https://github.com/alacritty/alacritty-theme $HOME/.config/alacritty/alacritty-theme
+
+  ok "alacritty"
+}
+
+# install tmux configurations.
+install_tmux() {
+  install "tmux"
+
+  link_file $PWD/tmux/tmux.conf $HOME/.tmux.conf
+
+  ok "tmux"
+}
+
+# install starship configurations.
+install_starship() {
+  install "starship"
+
+  link_file $PWD/starship/starship.toml $HOME/.config/starship.toml
+
+  ok "starship"
+}
 
 # install shells
 install_shell() {
-	if [[ ! -d "$SHELL_DIR" ]]; then
-		mkdir $SHELL_DIR
-	fi
+  if [[ ! -d "$SHELL_DIR" ]]; then
+    mkdir $SHELL_DIR
+  fi
 
-	for file in shell/*; do
-		if [[ -f "$file" ]]; then
-			local filename src dst
-			filename=$(basename "$file")
-			src=$(realpath "$file")
-			dst="$SHELL_DIR/$filename"
+  for file in shell/*; do
+    if [[ -f "$file" ]]; then
+      local filename src dst
+      filename=$(basename "$file")
+      src=$(realpath "$file")
+      dst="$SHELL_DIR/$filename"
 
-			if [[ -e "$dst" ]]; then
-				if ask "${filename} already exists, overwrite?"; then
-					ln -sf $src $dst
-				fi
-			else
-				ln -s $src $dst
-			fi
+      if [[ -e "$dst" ]]; then
+        if ask "${filename} already exists, overwrite?"; then
+          ln -sf $src $dst
+        fi
+      else
+        ln -s $src $dst
+      fi
 
-			if ! $(grep ".shell/$filename" $SH >/dev/null 2>&1); then
-				echo "source ~/.shell/$filename" >>"$SH"
-			fi
-		fi
-	done
+      if ! $(grep ".shell/$filename" $SH >/dev/null 2>&1); then
+        echo "source ~/.shell/$filename" >>"$SH"
+      fi
+    fi
+  done
 
-	ok "shell"
+  ok "shell"
 }
 
 #
@@ -164,35 +145,35 @@ install_shell() {
 #
 
 case "$TARGET" in
-"vim")
-	install_vim
-	;;
-"vim_basic")
-	install_vim_basic
-	;;
-"nvim")
-	install_nvim
-	;;
-"alacritty")
-	install_alacritty
-	;;
-"tmux")
-	install_tmux
-	;;
-"starship")
-	install_starship
-	;;
-"shell")
-	install_shell
-	;;
-"all")
-	install_vim
-	install_nvim
-	install_alacritty
-	install_tmux
-	install_shell
-	;;
-*)
-	fail "Unsupported target: $TARGET"
-	;;
+  "vim")
+    install_vim
+    ;;
+  "vim_basic")
+    install_vim_basic
+    ;;
+  "nvim")
+    install_nvim
+    ;;
+  "alacritty")
+    install_alacritty
+    ;;
+  "tmux")
+    install_tmux
+    ;;
+  "starship")
+    install_starship
+    ;;
+  "shell")
+    install_shell
+    ;;
+  "all")
+    install_vim
+    install_nvim
+    install_alacritty
+    install_tmux
+    install_shell
+    ;;
+  *)
+    fail "Unsupported target: $TARGET"
+    ;;
 esac
--- utils.sh
+++ utils.sh
@@ -2,51 +2,112 @@
 # Print an info-level message.
 ###
 info() {
-	printf "\r[ \033[00;34m..\033[0m ] $1\n"
+  printf "\r[ \033[00;34m..\033[0m ] $1\n"
 }
 
 ###
 # Print a warning-level message.
 ###
 warn() {
-	printf "\r[ \033[0;33m??\033[0m ] $1\n"
+  printf "\r[ \033[0;33m??\033[0m ] $1\n"
 }
 
 ###
 # Print a success-level message.
 ###
 success() {
-	printf "\r\033[2K[ \033[00;32mOK\033[0m ] $1\n"
+  printf "\r\033[2K[ \033[00;32mOK\033[0m ] $1\n"
 }
 
 ###
 # Print a fail-level message and exit the process by 1.
 ###
 fail() {
-	printf "\r\033[2K[\033[0;31mFAIL\033[0m] $1\n"
-	echo ''
-	exit 1
+  printf "\r\033[2K[\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit 1
 }
 
 ###
-# Ask Y/n
+# Confirm Y/n
 #
-# @params $1 The prompt message.
+# @param $1 The prompt message.
 # @returns {Boolean}
 #
 # @example
-#  if ask "already exists, overwrite?"; then
+#  if confirm "already exists, overwrite?"; then
 #    echo "something"
 #  fi
 #
 ###
-ask() {
-	read -p "$1 (Y/n): " resp
-	if [ -z "$resp" ]; then
-		response_lc="y" # empty is Yes
-	else
-		response_lc=$(echo "$resp" | tr '[:upper:]' '[:lower:]') # case insensitive
-	fi
+confirm() {
+  read -p "$1 (Y/n): " resp
 
-	[ "$response_lc" = "y" ]
+  if [ -z "$resp" ]; then
+    response_lc="y" # empty is Yes
+  else
+    response_lc=$(echo "$resp" | tr '[:upper:]' '[:lower:]') # case insensitive
+  fi
+
+  [ "$response_lc" = "y" ]
+}
+
+###
+# Link files
+#
+# @param $1 The source file path you want to link.
+# @param $2 The destination file path you want to link to.
+###
+link_file() {
+  info "Linking files: $1 => $2"
+
+  local src=$1 dst=$2
+
+  local action= overwrite= backup= skip=
+
+  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]; then
+    local currentSrc="$(readlink $dst)"
+
+    if [ "$currentSrc" == "$src" ]; then
+      skip=true
+    else
+      info "File already exists: $dst ($(basename "$src")), what do you want to do?\n\
+              [s]kip, [o]verwrite, [b]ackup?"
+      read -n 1 action </dev/tty
+
+      case "$action" in
+        o)
+          overwrite=true
+          ;;
+        b)
+          backup=true
+          ;;
+        s)
+          skip=true
+          ;;
+        *) ;;
+      esac
+    fi
+
+    if [ "$overwrite" == "true" ]; then
+      rm -rf "$dst"
+      success "removed $dst"
+    fi
+
+    if [ "$backup" == "true" ]; then
+      mv "$dst" "${dst}.bak"
+      success "moved $dst to ${dst}.bak"
+    fi
+
+    if [ "$skip" == "true" ]; then
+      success "skipped $src"
+    fi
+  fi
+
+  if [ "$skip" != "true" ]; then # "false" or empty
+    ln -s "$1" "$2"
+    success "linked $1 to $2"
+  fi
+
+  success "Files linked"
 }
