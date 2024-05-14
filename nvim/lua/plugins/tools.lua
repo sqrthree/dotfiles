ebf67c3b5274e3cbe7e2825ffea7c1a0122143b0
Lin He <linhe0x0@gmail.com>
2024-05-15 01:28:22 +0800

    feat(nvim): add support for mappings for Gitsigns

--- nvim/lua/plugins/tools.lua
+++ nvim/lua/plugins/tools.lua
@@ -2,6 +2,19 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     event = "VeryLazy",
-    config = true,
+    config = function()
+      require('gitsigns').setup({
+        on_attach = function(bufnr)
+          local function map(mode, lhs, rhs, opts)
+              opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
+              vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr=true })
+          map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr=true })
+        end
+      })
+    end
   },
 }
