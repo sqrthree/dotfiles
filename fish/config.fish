ac335f5e3b067558b269778a1bebe5fe84a0d140
Lin He <linhe0x0@gmail.com>
2024-07-18 23:57:06 +0800

    fix(fish): resolve errors

--- fish/config.fish
+++ fish/config.fish
@@ -2,28 +2,28 @@
 # Environments
 ###
 # You may need to manually set your language environment
-set -gxU LANG 'en_US.UTF-8'
+set -gx LANG 'en_US.UTF-8'
 
 # Proxy brew source with tsinghua.
 # Doc: https://mirrors.tuna.tsinghua.edu.cn/help/homebrew/
-set -gxU HOMEBREW_API_DOMAIN 'https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api'
-set -gxU HOMEBREW_BOTTLE_DOMAIN 'https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles'
-set -gxU HOMEBREW_BREW_GIT_REMOTE 'https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git'
+set -gx HOMEBREW_API_DOMAIN 'https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api'
+set -gx HOMEBREW_BOTTLE_DOMAIN 'https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles'
+set -gx HOMEBREW_BREW_GIT_REMOTE 'https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git'
 
 # Set theme for bat syntax highlighting.
 # Or just call bat with the --theme=DarkNeon option
-set -gxU BAT_THEME 'DarkNeon'
+set -gx BAT_THEME 'DarkNeon'
 
 # Set fzf layout: https://github.com/junegunn/fzf#layout
-set -gxU FZF_DEFAULT_OPTS '--height 40% --layout=reverse --info=inline --border --preview="bat --color=always --style=numbers --line-range=:100 {}" --preview-window="right:60%" --bind "up:preview-up,down:preview-down"'
-set -gxU FZF_DEFAULT_COMMAND 'fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
+set -gx FZF_DEFAULT_OPTS '--height 40% --layout=reverse --info=inline --border --preview="bat --color=always --style=numbers --line-range=:100 {}" --preview-window="right:60%" --bind "up:preview-up,down:preview-down"'
+set -gx FZF_DEFAULT_COMMAND 'fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
 
 # Bun
-set -gxU BUN_INSTALL "$HOME/.bun"
-set -gxU PATH $BUN_INSTALL/bin $PATH
+set -gx BUN_INSTALL "$HOME/.bun"
+set -gx PATH $BUN_INSTALL/bin $PATH
 
 # Setup go path
-set -gxU GOPATH "$HOME/.go"
+set -gx GOPATH "$HOME/.go"
 
 ###
 # alias
