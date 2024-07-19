4095c5fa6c1aee6d0d944bc362bf7c31bf5ef97e
Lin He <linhe0x0@gmail.com>
2024-07-19 11:15:49 +0800

    feat(git): add support for git

---
+++ git/install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ "${TRACE-0}" == "1" ]]; then
+  set -o xtrace
+fi
+
+cd "$(dirname "$0")"
+
+# source utils
+source "../utils.sh"
+
+# install git configurations.
+install_git() {
+  install "git"
+
+  link_file $PWD/gitconfig $HOME/.gitconfig
+  touch $HOME/.gitconfig.local
+  mkdir -p $HOME/.config/git
+  link_file $PWD/gitignore $HOME/.config/git/gitignore
+  link_file $PWD/gitmessage $HOME/.config/git/gitmessage
+
+  ok "git"
+}
+
+install_git
