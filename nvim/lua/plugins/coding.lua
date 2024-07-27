eaf9c95a443be04d71755801931bf5f37d063b28
Lin He <linhe0x0@gmail.com>
2024-07-27 11:58:30 +0800

    fix(nvim): adjust mappings to remove `whick-key` warnings

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -207,47 +207,58 @@ return {
     event = "VeryLazy",
     opts = {
       options = {
+        -- Whether to ignore blank lines when commenting
+        ignore_blank_line = true,
+
+        -- Whether to recognize as comment only lines without indent
+        start_of_line = true,
+
+        -- Function to compute custom 'commentstring'
         custom_commentstring = function()
           return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
         end,
       },
+
+      mappings = {
+        -- Toggle comment (like `gcip` - comment inner paragraph) for both
+        -- Normal and Visual modes
+        comment = 'gc',
+
+        -- Toggle comment on current line
+        comment_line = 'gcc',
+
+        -- Toggle comment on visual selection
+        comment_visual = 'gc',
+
+        -- Define 'comment' textobject (like `dgc` - delete whole comment block)
+        -- Works also in Visual mode if mapping differs from `comment_visual`
+        textobject = 'gc',
+      },
     },
     keys = {
-      { "<leader>c",  "gcc", desc = "Toggle comment on current line", remap = true,  },
+      { "<leader>c", "gcc", desc = "Toggle comment on current line", remap = true, },
     },
   },
   {
     "echasnovski/mini.surround",
     event = "VeryLazy",
-    keys = function(_, keys)
-      -- Populate the keys based on the user's options
-      local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
-      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-      local mappings = {
-        { opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
-        { opts.mappings.delete, desc = "Delete surrounding" },
-        { opts.mappings.find, desc = "Find right surrounding" },
-        { opts.mappings.find_left, desc = "Find left surrounding" },
-        { opts.mappings.highlight, desc = "Highlight surrounding" },
-        { opts.mappings.replace, desc = "Replace surrounding" },
-        { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
-      }
-      mappings = vim.tbl_filter(function(m)
-        return m[1] and #m[1] > 0
-      end, mappings)
-      return vim.list_extend(mappings, keys)
-    end,
-    opts = {
-      mappings = {
-        add = "gsa", -- Add surrounding in Normal and Visual modes
-        delete = "gsd", -- Delete surrounding
-        find = "gsf", -- Find surrounding (to the right)
-        find_left = "gsF", -- Find surrounding (to the left)
-        highlight = "gsh", -- Highlight surrounding
-        replace = "gsr", -- Replace surrounding
-        update_n_lines = "gsn", -- Update `n_lines`
-      },
-    },
+    config = function()
+      require('mini.surround').setup({
+        mappings = {
+          add = "gsa",            -- Add surrounding in Normal and Visual modes
+          delete = "gsd",         -- Delete surrounding
+          find = "gsf",           -- Find surrounding (to the right)
+          find_left = "gsF",      -- Find surrounding (to the left)
+          highlight = "gsh",      -- Highlight surrounding
+          replace = "gsr",        -- Replace surrounding
+          update_n_lines = "gsn", -- Update `n_lines`
+
+          suffix_last = 'l',      -- Suffix to search with "prev" method
+          suffix_next = 'n',      -- Suffix to search with "next" method
+        },
+
+      })
+    end
   },
   {
     "folke/todo-comments.nvim",
--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -95,7 +95,7 @@ return {
           smart_rename = {
             enable = true,
             keymaps = {
-              smart_rename = "grr",
+              smart_rename = "gsr",
             },
           },
           navigation = {
@@ -268,31 +268,40 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
-      plugins = { spelling = true },
-      defaults = {
-        mode = { "n", "v" },
-        ["g"] = { name = "+goto" },
-        ["gs"] = { name = "+surround" },
-        ["]"] = { name = "+next" },
-        ["["] = { name = "+prev" },
-        ["<leader><tab>"] = { name = "+tabs" },
-        ["<leader>b"] = { name = "+buffer" },
-        ["<leader>c"] = { name = "+code" },
-        ["<leader>f"] = { name = "+file/find" },
-        ["<leader>g"] = { name = "+git" },
-        ["<leader>gh"] = { name = "+hunks" },
-        ["<leader>q"] = { name = "+quit/session" },
-        ["<leader>s"] = { name = "+search" },
-        ["<leader>u"] = { name = "+ui" },
-        ["<leader>w"] = { name = "+windows" },
-        ["<leader>x"] = { name = "+diagnostics/quickfix" },
+      --   plugins = { spelling = true },
+      --   defaults = {
+      --     -- mode = { "n", "v" },
+      --     -- ["g"] = { name = "+goto" },
+      --     -- ["gs"] = { name = "+surround" },
+      --     -- ["]"] = { name = "+next" },
+      --     -- ["["] = { name = "+prev" },
+      --     -- ["<leader><tab>"] = { name = "+tabs" },
+      --     -- ["<leader>b"] = { name = "+buffer" },
+      --     -- ["<leader>c"] = { name = "+code" },
+      --     -- ["<leader>f"] = { name = "+file/find" },
+      --     -- ["<leader>g"] = { name = "+git" },
+      --     -- ["<leader>gh"] = { name = "+hunks" },
+      --     -- ["<leader>q"] = { name = "+quit/session" },
+      --     -- ["<leader>s"] = { name = "+search" },
+      --     -- ["<leader>u"] = { name = "+ui" },
+      --     -- ["<leader>w"] = { name = "+windows" },
+      --     -- ["<leader>x"] = { name = "+diagnostics/quickfix" },
+      --   },
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
       },
     },
-    config = function(_, opts)
-      local wk = require("which-key")
-      wk.setup(opts)
-      wk.register(opts.defaults)
-    end,
+    -- config = function(_, opts)
+    --   local wk = require("which-key")
+    --   wk.setup(opts)
+    --   wk.register(opts.defaults)
+    -- end,
   },
   {
     "folke/flash.nvim",
