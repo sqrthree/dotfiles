c6bc59e8a92fa3dc8041921a87ffdd141a19ec47
Lin He <linhe0x0@gmail.com>
2024-07-27 12:05:43 +0800

    chore(nvim): remove specific versions so that the default version is applied to every plugin

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -442,7 +442,6 @@ return {
     's1n7ax/nvim-window-picker',
     name = 'window-picker',
     event = 'VeryLazy',
-    version = '2.*',
     config = function()
       require("window-picker").setup({
         filter_rules = {
--- nvim/lua/plugins/lang.lua
+++ nvim/lua/plugins/lang.lua
@@ -3,6 +3,5 @@ return {
   {
     "b0o/SchemaStore.nvim",
     lazy = true,
-    version = false, -- last release is way too old
   },
 }
