2fd1d9542710c493e2f0e02cbdd7ca3cd5a542d0
Lin He <linhe0x0@gmail.com>
2024-08-08 10:42:20 +0800

    fix(nvim): install `tsserver` by default

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -141,6 +141,7 @@ return {
       -- ALl available LSP servers can be found at the following url:
       -- https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
       ensure_installed = {
+        "tsserver",
         "volar",
         "jsonls",
         "bashls",
