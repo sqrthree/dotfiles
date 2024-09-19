c3257cea5b655aed0bee41dd00c36214dd182b0c
Lin He <linhe0x0@gmail.com>
2024-09-19 10:49:32 +0800

    chore(nvim): adjust completion behavior

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -27,14 +27,24 @@ return {
             require("luasnip").lsp_expand(args.body)
           end,
         },
+        preselect = cmp.PreselectMode.Item,
+        completion = {
+          completeopt = 'menu,menuone,preview'
+        },
         mapping = cmp.mapping.preset.insert({
           ["<C-d>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          },
+          ["<CR>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.confirm({
+                behavior = cmp.ConfirmBehavior.Insert,
+                select = true,
+              })
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
           ["<C-n>"] = cmp.mapping.select_next_item({
             behavior = cmp.SelectBehavior.Insert
           }),
